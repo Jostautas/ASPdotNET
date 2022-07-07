98224b248ddda94f4b7c5c768114aabfa97e5da6
Jostautas <jostautas.sakas@gmail.com>
2022-07-07 21:29:02 +0300

    OMG IT WERKZ

--- WebApplication1/Pages/Number.cshtml.cs
+++ WebApplication1/Pages/Number.cshtml.cs
@@ -34,15 +34,7 @@ namespace WebApplication1.Pages
         
         /*public void Button1_Click(object sender, EventArgs e)
         {
-            using (SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=aspnet-WebApplication1-53bc9b9d-9d6a-45d4-8429-2a2761773502;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
-            {
-                using (SqlCommand cmdSQL = new SqlCommand("SELECT COUNT(*) FROM AspNetUsers", conn))
-                {
-                    conn.Open();
-                    int? RowCount = (int?)cmdSQL.ExecuteScalar();
-                    TextBox1.Text = RowCount.ToString();
-                }
-            }
+            
 
         }*/
     }
